9f2a33d5ac5c72c253aad6cf589d33b77c7348ed
soffirm <soffir3003@gmail.com>
2021-11-14 12:54:08 +0530

    new statement added

--- src/org/andrapradesh/AndhraPradesh.java
+++ src/org/andrapradesh/AndhraPradesh.java
@@ -3,5 +3,16 @@ package org.andrapradesh;
 public class AndhraPradesh {
 	public void telugu() {
 System.out.println("telugu");
+//new lines added. 
+
+///////
+
+int a= 10;
+int b=20;
+
+System.out.println(a+b);
+
+
+//////////////////////////
 	}
 }
