34334143b29db054950e7b5b9da6a134b45ff823
soffirm <soffir3003@gmail.com>
2021-12-03 13:20:12 +0530

    added new sample method

--- src/org/andrapradesh/AndhraPradesh.java
+++ src/org/andrapradesh/AndhraPradesh.java
@@ -1,18 +1,24 @@
 package org.andrapradesh;
 
 public class AndhraPradesh {
+	
 	public void telugu() {
-System.out.println("telugu");
-//new lines added. 
-
-///////
-
-int a= 10;
-int b=20;
-
-System.out.println(a+b);
-
-
-//////////////////////////
+	System.out.println("telugu");
+	int a= 10;
+	int b=20;
+	System.out.println(a+b);
+	}
+	
+	private void sampleMethod() {
+		System.out.println("trying to make conflict");
+		System.out.println("Let's see");
+		System.out.println("Stefan salvatore");
+		System.out.println("Damon salvatore");
+	}
+	
+	public static void main(String[] args) {
+		AndhraPradesh a= new AndhraPradesh();
+		a.sampleMethod();
+		a.telugu();
 	}
 }
